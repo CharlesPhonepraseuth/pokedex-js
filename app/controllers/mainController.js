250f57eb5039b68fec75337ef87215b1817d411e
CharlesPhonepraseuth <charles.phonepraseuth@gmail.com>
2020-12-12 15:06:31 +0100

    wip : controller + model

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -2,7 +2,7 @@ const dataMapper = require('../dataMapper');
 
 const mainController = {
 
-    homePage:  (req, res) => {
+    homePage: (req, res) => {
         dataMapper.getAllPokemons((err, data) => {
             // if we found an error, we log this one, and we send back to navigator
             if (err) {
@@ -13,6 +13,26 @@ const mainController = {
             // if everything is okay, we send data from data.rows to home view
             res.render('home', {
                 pokemons: data.rows
+            });
+        });
+    },
+
+    pokemonPage: (req, res) => {
+        const pokemonNum = req.params.numero;
+
+        dataMapper.getPokemonDetails(pokemonNum, (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send(err);
+            }
+            // to avoid complex query, we do 2 separate queries
+            dataMapper.getPokemonTypes(pokemonNum, (err2, data2) => {
+                if (err2) {
+                    console.log(err2);
+                    return res.status(500).send(err2);
+                };
+                console.log('POKEMON :: ', data.rows[0]);
+                console.log('TYPES :: ', data2.rows);
             });
         });
     }
--- app/dataMapper.js
+++ app/dataMapper.js
@@ -5,6 +5,22 @@ const dataMapper = {
     getAllPokemons: (callback) => {
         const myQuery = "SELECT * FROM pokemon";
         client.query(myQuery, callback);
+    },
+
+    getPokemonDetails: (numero, callback) => {
+        const myQuery = `SELECT * FROM pokemon WHERE numero=$1`;
+        const values = [numero];
+        client.query(myQuery, values, callback);
+    },
+
+    getPokemonTypes: (numero, callback) => {
+        const myQuery = `
+            SELECT *
+            FROM type t
+            JOIN pokemon_type pt ON t.id = pt.type_id
+            WHERE pt.pokemon_numero=$1`;
+        const values = [numero];
+        client.query(myQuery, values, callback);
     }
 
 };
