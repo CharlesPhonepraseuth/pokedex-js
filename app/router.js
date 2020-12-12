5dcf66f3d4af41e47aa908ad7228bf2bcb90aebb
CharlesPhonepraseuth <charles.phonepraseuth@gmail.com>
2020-12-12 15:20:36 +0100

    wip : routeur

--- app/router.js
+++ app/router.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    mainController
+    mainController,
+    typeController
 } = require('./controllers');
 
 router.get('/', mainController.homePage);
 router.get('/pokemon/:numero', mainController.pokemonPage);
+
+router.get('/types', typeController.typesPage);
 
 module.exports = router;
