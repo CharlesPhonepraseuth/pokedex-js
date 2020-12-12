f8c704cd9bfa3b164f2bf67f8868deab081e99c6
CharlesPhonepraseuth <charles.phonepraseuth@gmail.com>
2020-12-12 16:14:47 +0100

    wip : controller

--- app/controllers/teamController.js
+++ app/controllers/teamController.js
@@ -1,8 +1,24 @@
+const dataMapper = require('../dataMapper');
+
 const teamController = {
 
     teamPage: (req, res) => {
         res.render('team',{
             pokemons: req.session.team
+        });
+    },
+
+    addToTeam: (req, res) => {
+        const pokemonNum = req.params.numero;
+
+        dataMapper.getPokemonDetails(pokemonNum, (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send(err);
+            };
+            
+            req.session.team.push(data.rows[0]);
+            res.redirect('/team');
         });
     }
 
