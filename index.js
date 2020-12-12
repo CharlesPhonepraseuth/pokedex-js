22efc693963849f2dddd9a1786facd4f6e145c0e
CharlesPhonepraseuth <charles.phonepraseuth@gmail.com>
2020-12-12 16:01:39 +0100

    wip : session settup

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.set('views', 'app/views');
 // static files
 app.use(express.static('public'));
 
+// we add session into our project
+const session = require('express-session');
+app.use(session({
+  secret: 'a random string',
+  saveUninitialized: true,
+  resave: true
+}));
+// and we create a small middleware to make sure our 'team' list exist
+app.use( (req, res, next) => {
+  if(!req.session.team) {
+    req.session.team = [];
+  };
+  next();
+});
+
 // we settup router 
 const router = require('./app/router');
 app.use(router);
